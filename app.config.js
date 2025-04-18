944fb182acc21847584fc88e40010d3aadadd657
Your Name <your@email.com>
2025-04-19 00:37:36 +0700

    Fix web deployment configuration

---
+++ app.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  expo: {
+    name: "expo-app",
+    slug: "expo-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      }
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+      build: {
+        publicPath: "/cateringta/"
+      }
+    },
+    plugins: ["expo-router"],
+    experiments: {
+      typedRoutes: true
+    }
+  }
+}; 
